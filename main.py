216f766a58812b3e204c0673d3ad68f1a07bf152
VladymyrLu <113236960+VladymyrLu@users.noreply.github.com>
2022-09-15 11:23:02 +0200

    add main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-a = input('Сторона А  ')
-a = int(a)
-h = input('Высота h  ')
-h = int(h)
-print('S треугольника равна ', 0.5 * a * h)
+# a = input('Сторона А  ')
+# a = int(a)
+# h = input('Высота h  ')
+# h = int(h)
+# print('S треугольника равна ', 0.5 * a * h)
+
+
+
+print('yyy')
+
 
