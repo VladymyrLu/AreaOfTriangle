677db21f45a85741de2c54e1a424a6955eb739d8
VladymyrLu <113236960+VladymyrLu@users.noreply.github.com>
2022-09-16 13:24:09 +0200

    add dz5

---
+++ dz5/dz5_2.py
@@ -0,0 +1,11 @@
+a = int(input('переменная а :  '))
+b = int(input('переменная b :  '))
+c = int(input('переменная c :  '))
+if a>b and a>c:
+    print(f'max:{a}')
+elif b>a and b>c:
+    print(f'max: {b}')
+elif c>a and c>b:
+    print(f'max:{c}')
+
+
