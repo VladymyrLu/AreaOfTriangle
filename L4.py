2adbbcbaebe4bf5d24b0dbbae13115c16a2289a8
VladymyrLu <113236960+VladymyrLu@users.noreply.github.com>
2022-09-16 13:23:32 +0200

    add dz5

--- L4.py
+++ L4.py
@@ -1,11 +1,32 @@
-print(5 < 10)
-print(5 <= 10)
-print(5 > 10)
-print(5 >= 10)
-print(5 == 10)
-print(5 != 10)
+# number = 11
+
+# print(number <= 10)
+# print(1 <= number <= 15)
+# print('ll' in 'Hello')
+#
+#
+# if password == '123' and 'Vasya' in 'Vasya@gmail.com':
+#     print(f'Hello Vasya@gmail.com')
+# if password == '123' and 'Vasya' in 'Vasya123@gmail.com':
+#     print(f'Hello Vasya123@gmail.com')
 
 
+# if number == 11 :
+#     print('ravno 11')
+# else:
+#     print('????')
+# print('end')
 
 
-print('222')
+number = 23
+guess = int(input('Введите целое число : '))
+
+if guess == number:
+    print('Поздравляю, вы угадали,')  # Начало нового блока
+    print('(хотя и не выиграли никакого приза!)')  # Конец нового блока
+elif guess < number:
+    print('Нет, загаданное число немного больше этого.')  # Ещё один блок
+    # Внутри блока вы можете выполнять всё, что угодно ...
+else:
+    print('Нет, загаданное число немного меньше этого.')
+    # чтобы попасть сюда, guess должно быть больше, чем number
---
+++ dz5/dz5.py
@@ -0,0 +1,17 @@
+a = int(input('переменная а :  '))
+b = int(input('переменная b :  '))
+c = int(input('переменная c :  '))
+if a%3==0 and a>10 and b%3==0 and b>10 and c>10:
+    print('yes')
+else:
+    print('no')
+
+
+
+
+
+
+
+
+
+
