08ca887d9e6b32e7429bbec8c7bb09d1a1af0121
VladymyrLu <113236960+VladymyrLu@users.noreply.github.com>
2022-10-01 09:11:45 +0200

    add  dz_8

--- dz_8/dz_8_3.py
+++ dz_8/dz_8_3.py
@@ -1,41 +1,41 @@
- # 3/Напишите программу где пользователь вводит пароль, а программа проверяет сложность пароля и выводит свой результат в оценочной шкале от 1 до 5.
+import \
+    string  # 3/Напишите программу где пользователь вводит пароль, а программа проверяет сложность пароля и выводит свой результат в оценочной шкале от 1 до 5.
 # 1 - у пользователя пароль == 'qwerty' or 'admin' или пароль пустой
 # 2 - у пользователя все буквы в нижнем регистре
 # 3 - у пользователя есть буквы в нижнем регистре и цифры
 # 4 - у пользователя есть цифры, буквы нижнего и верхнего регистра
 # 5 - у пользователя есть цифры, буквы нижнего и верхнего регистра, спец. символы и длинна пароля больше 8 символов
 
-# paroll = input('Введите пароль  ')
-# ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
-# ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-# digits = '0123456789'
-# punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
-# if 'qwerty' == paroll or 'admin' == paroll or '' == paroll:
-#     print('1')
-# elif paroll == ascii_lowercase:
-#     print('2')
-
-
-p = input(("plz enter a password to check it's strength"))
-
-upper_case = 0
-lower_case = 0
-number = 0
+paroll = input('введите стоку ')
+ascii_lowercase = 0
+ascii_uppercase = 0
+digits = 0
+ispace = 0
+special = 0
 symbol = 0
-
-for i in p:
-    if i.isupper():
-        upper_case += 1
-    elif i.islower():
-        lower_case += 1
-    elif i.isdigit():
-        number += 1
+import string
+for symbol in paroll:
+    if symbol.isupper():
+        ascii_uppercase += 1
+    elif symbol.islower():
+        ascii_lowercase += 1
+    elif symbol.isdigit():
+        digits += 1
+    elif symbol.isspace():
+        ispace += 1
+    elif symbol in string.punctuation:
+        special += 1
     else:
         symbol += 1
-
-if len (p) <= 6:
-    print("This is a weak password  ")
-elif upper_case > 0 and lower_case > 0 and number > 0 and symbol > 0:
-    print ("Good")
+if 'qwerty' == paroll or 'admin' == paroll or '' == paroll:
+    print('1')
+elif len(paroll) > 8 and ascii_uppercase > 0 and ascii_lowercase and special > 0 and digits > 0:
+    print('5')
+elif ascii_uppercase > 0 and ascii_lowercase and digits > 0:
+    print('4')
+elif ascii_lowercase > 0 and digits > 0:
+    print('3')
+elif ascii_lowercase > 0:
+    print('2')
 else:
-    print ("Too Weak")
+    print('Введите пароль заново')
